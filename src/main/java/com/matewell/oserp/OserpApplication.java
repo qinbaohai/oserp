26579fef0445d2265caa10520489af650585fd27
秦宝海 <qinbaohai@longshine.com>
2020-01-09 20:06:10 +0800

    first commit

---
+++ src/main/java/com/matewell/oserp/OserpApplication.java
@@ -0,0 +1,13 @@
+package com.matewell.oserp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OserpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OserpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matewell/oserp/OserpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matewell.oserp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OserpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
